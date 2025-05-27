e497fc43dc3e13baa0009b5a27bfc4b03fe8739b
omer-habib-blocship <omer.habib@blocship.io>
2025-05-27 12:56:05 +0500

    Implement navigation structure with WidgetTree and ValueNotifier for page management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/views/widget_tree.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,20 +23,7 @@ class _MyAppState extends State<MyApp> {
           brightness: Brightness.dark,
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter App')),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-          onDestinationSelected:
-              (value) => setState(() {
-                selectedIndex = value;
-              }),
-          selectedIndex: selectedIndex,
-        ),
-      ),
+      home: WidgetTree(),
     );
   }
 }
